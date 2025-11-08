eaf88230e19c41162bb29744b380c5efb5db129c
Marek-VF <marek.bugla@die-vielfalter.de>
2025-11-08 12:54:39 +0100

    clean branch

--- config.php
+++ config.php
@@ -1,13 +1,13 @@
 <?php
 $baseUrl = 'https://vielfalter.digital/api-monday/ecommagent';
-
+$webhookBearerToken = 'changeme';
 return [
     'base_url'         => $baseUrl,
     'asset_base_url'   => $baseUrl . '/assets',
     'upload_dir'       => __DIR__ . '/uploads/',
     'data_file'        => __DIR__ . '/data.json',
     'workflow_webhook' => 'https://tex305agency.app.n8n.cloud/webhook-test/9a217ab8-47fa-452c-9c65-fa7874a14fdd',
-    'receiver_api_token'      => 'change-me',
+    'receiver_api_token'      => $webhookBearerToken,
     'receiver_api_allowed_ips' => [],
     'db'               => [
         'dsn'      => 'mysql:host=localhost;dbname=ecommagent;charset=utf8mb4',
